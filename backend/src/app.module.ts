e0b04ffeffead68c351c990bbb24ebcb7a801242
haimv <haimv1@fpt.com>
2026-06-15 16:10:30 +0700

    feat(backend): typeorm datasource + mariadb connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/modules/**/entities/*.entity.{ts,js}'],
+      synchronize: false,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ backend/src/data-source.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['src/modules/**/entities/*.entity.ts'],
+  migrations: ['src/database/migrations/*.ts'],
+  synchronize: false,
+})
