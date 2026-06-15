08291b155dc8fa5204e3eeaddf2cfc6fa2510b59
haimv <haimv1@fpt.com>
2026-06-15 16:26:36 +0700

    feat(backend): bootstrap with global pipe/filter/interceptor, cors, swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+import { ResponseInterceptor } from './common/interceptors/response.interceptor'
+import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+  app.enableCors({ origin: ['http://localhost:5173', 'http://localhost:5174'] })
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+  app.useGlobalInterceptors(new ResponseInterceptor())
+  app.useGlobalFilters(new HttpExceptionFilter())
+  const cfg = new DocumentBuilder().setTitle('WorkShop Pro API').setVersion('1.0').build()
+  SwaggerModule.setup('api/docs', app, SwaggerModule.createDocument(app, cfg))
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
